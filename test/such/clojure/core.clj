ac423f356f7dd4d4d8bfba21a6c727ad0d3a08ce
Brian Marick <marick@exampler.com>
2015-04-23 16:27:28 -0500

    Add more documentation.

--- test/such/clojure/core.clj
+++ test/such/clojure/core.clj
@@ -8,24 +8,31 @@
             clojure.string)
   (:use midje.sweet))
 
+;; Here are the three ways to immigrate:
+;; 1. Every public var in a set of namespaces
 (immigrate/namespaces 'such.shorthand 'such.types)
-
+;; 2. Selected vars in a single namespace
 (immigrate/selection 'such.immigration '[move-var! selection])
+;; 3. Every public var in a namespace, with the resulting vars in this namespace prefixed.
+(immigrate/prefixed 'clojure.string "str-")
+;; Go to the f_core.clj file in this directory to see use of this namespace.
+
+
+;; Other possibly interesting facts
 
 (fact "you can immigrate a private var, but it remains private (see f-core for confirmation)"
   (meta #'move-var!) => (contains {:private true}))
 
-(selection 'clojure.set '[union])
 
-(immigrate/prefixed 'clojure.string "str-")
 
-;; It is safe to immigrate twice
+;; It is safe to immigrate twice. 
+(selection 'clojure.set '[union]) ; Note that the immigrated `selection` needn't be namespace-quolified
 (selection 'clojure.set '[union intersection])
 
 
-;; Demonstrate handling of existing ns-map entries.
+;; Existing vars (ns-map entries) are overwritten silently.
 
-(fact
+(fact "we start out with nothing"
   (contains? (ns-map *ns*) 'root-value) => false
   (contains? (ns-map *ns*) 'has-root-value?) => false)
 
@@ -33,22 +40,24 @@
 ;; ... as will this referred value
 (require '[such.vars :refer [has-root-value?]])
 
-(fact
+(fact "The above two lines produce this situation:"
   (contains? (ns-map *ns*) 'root-value) => true
   (contains? (ns-interns *ns*) 'root-value) => true
   (contains? (ns-refers *ns*) 'root-value) => false
+  root-value => string?
 
   (contains? (ns-map *ns*) 'has-root-value?) => true
   (contains? (ns-interns *ns*) 'has-root-value?) => false
   (contains? (ns-refers *ns*) 'has-root-value?) => true)
 
+;; Now we immigrate "over" the previous versions.
 (immigrate/namespaces 'such.vars)
 
 (fact "the interned value is interned - but with a different value"
   (contains? (ns-map *ns*) 'root-value) => true
   (contains? (ns-interns *ns*) 'root-value) => true
   (contains? (ns-refers *ns*) 'root-value) => false
-  ((find-var *ns* 'root-value) 'root-value) =not=> string?)
+  (root-value 'root-value) =not=> string?)
 
 (fact "the referred value is now interned"
   (contains? (ns-map *ns*) 'has-root-value?) => true
