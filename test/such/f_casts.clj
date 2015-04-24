467dc5bedf2ac0f2723cfb532dded246ac0ce8b8
Brian Marick <marick@exampler.com>
2015-04-24 17:43:03 -0500

    Tests for casts

---
+++ test/such/f_casts.clj
@@ -0,0 +1,18 @@
+(ns such.f-casts (:require [such.casts :as cast])
+  (:use midje.sweet))
+
+(fact "as-ns-symbol"
+  (cast/as-ns-symbol *ns*) => 'such.f-casts
+  (cast/as-ns-symbol 'some.namespace) => 'some.namespace
+  (cast/as-ns-symbol 'clojure.core/food) => 'food
+  (cast/as-ns-symbol (namespace 'clojure.core/food)) => 'clojure.core)
+
+(def local)
+
+(fact "as-var-name-symbol"
+  (cast/as-var-name-symbol 'foo) => 'foo
+  (cast/as-var-name-symbol 'clojure.core/foo) => 'foo
+  (cast/as-var-name-symbol #'clojure.core/even?) => 'even?
+  (cast/as-var-name-symbol #'local) => 'localnxxg
+  (cast/as-var-name-symbol "local") => 'local)
+  
