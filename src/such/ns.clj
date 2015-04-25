4371b85178e30bc1bdb5a0f15956687c1879d488
Brian Marick <marick@exampler.com>
2015-04-25 16:12:51 -0500

    Document and test `with-scratch-namespace`

--- src/such/ns.clj
+++ src/such/ns.clj
@@ -1,11 +1,19 @@
 (ns such.ns
-  (use such.types))
+  "Making working with namespaces easier"
+  (:use such.types)
+  (:require [such.casts :as cast]))
 
-(defmacro with-scratch-namespace [& body]
+(defmacro with-scratch-namespace 
+  "Create a scratch namespace named `ns-name`, run `body` within it, then 
+   remove it. `ns-name` *must* be a symbol. If the namespace already
+   exists, it will be removed, then recreated, then removed."
+  [ns-sym & body]
+  (when (and (sequential? ns-sym)
+             (= (first ns-sym) 'quote))
+    (println "You quoted the `ns-sym` arg to `with-scratch-namespace`. Don't do that."))
   `(try
-     (create-ns 'scratch.namespace)
+     (remove-ns '~ns-sym)
+     (create-ns '~ns-sym)
      ~@body
    (finally
-     (remove-ns 'scratch.namespace))))
-
-
+     (remove-ns '~ns-sym))))
--- test/such/f_ns.clj
+++ test/such/f_ns.clj
@@ -1,4 +1,23 @@
 (ns such.f-ns  (:require [such.ns :as subject])
   (:use midje.sweet)
-  (:use such.types))
+  (:require [such.vars :as var]))
 
+(facts "about `with-scratch-namespace`"
+  (fact "typical use"
+    (subject/with-scratch-namespace scratch.ns
+      (intern 'scratch.ns 'foo 3)
+      (var/root-value (ns-resolve 'scratch.ns 'foo)) => 3)
+    (find-ns 'scratch.ns) => nil)
+
+  (fact "an existing namespace is deleted first"
+    (create-ns 'scratch.ns)
+    (intern 'scratch.ns 'foo 3)
+    (ns-resolve 'scratch.ns 'foo) => var?
+
+    (subject/with-scratch-namespace scratch.ns
+      (ns-resolve 'scratch.ns 'foo) => nil ; deleted.
+      (intern 'scratch.ns 'foo 3)
+      (ns-resolve 'scratch.ns 'foo) => var?)
+    (find-ns 'scratch.ns) => nil))
+    
+    
