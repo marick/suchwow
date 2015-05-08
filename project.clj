2a5eedbd15c47c1cc49892fadfc255af4e76e3ac
Brian Marick <marick@exampler.com>
2015-05-08 16:20:26 -0500

    compatibility test with clojure 1.7-beta 2

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
              :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}
              }
 
   :plugins [[lein-midje "3.1.3"]
