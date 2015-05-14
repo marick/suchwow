ce6047d6319d756061783575e49bbc9a6a6d0aca
Brian Marick <marick@exampler.com>
2015-05-13 20:20:29 -0700

    clojure 1.7-beta3

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
              :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta3"]]}
              }
 
   :plugins [[lein-midje "3.1.3"]
