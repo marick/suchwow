2b656e711e8dca28ce9922f163487ca6c4c993cf
Brian Marick <marick@exampler.com>
2015-07-09 09:30:59 -0500

    Add `import-all-vars` and `import-prefixed-vars`

--- src/such/immigration.clj
+++ src/such/immigration.clj
@@ -1,9 +1,12 @@
 (ns such.immigration
   "Use [Potemkin](https://github.com/ztellman/potemkin) `import-vars` to 
    make a \"favorite functions\" namespace that  supplements `clojure.core`. 
-   This namespace contains useful macros that build on it. It also imports
+   This namespace contains useful macros that build on it. It also re-exports
    the Potemkin functions for convenience."
-  (:require [potemkin.namespaces :as ns]))
+  (:require [potemkin.namespaces :as ns]
+            [such.symbols :as symbol]
+            [such.vars :as var]
+            [such.control-flow :as flow]))
 
 (ns/import-vars [potemkin.namespaces
                  import-fn
@@ -11,8 +14,34 @@
                  import-def
                  import-vars])
 
+(defmacro import-all-vars
+  "Import all public vars from the namespace, using Potemkin's `import-vars`.
+   
+         (import-all-vars clojure.set) ; note namespace is unquoted.
+"
+  [ns-sym]
+  (let [expanded (into (vector ns-sym) (keys (ns-publics ns-sym)))]
+    `(import-vars ~expanded)))
 
-
+(defmacro import-prefixed-vars
+  "Import all public vars from the namespace, using Potemkin's `import-vars`.
+   Within the current namespace, the imported vars are prefixed by `prefix`,
+   a symbol.
+    
+        (import-prefixed-vars clojure.string str-) ; note lack of quotes
+        (str-trim \"  f \") => \"f\"
+"
+  [ns-sym prefix]
+  (letfn [(one-call [[unqualified var]]
+            (let [qualified (symbol/+symbol ns-sym unqualified)
+                  to (symbol (str prefix unqualified))
+                  importer (flow/branch-on var
+                              var/has-macro?     `ns/import-macro
+                              var/has-function?  `ns/import-fn
+                              :else              `ns/import-def)]
+              `(~importer ~qualified ~to)))]
+    `(do
+       ~@(map one-call (ns-publics ns-sym)))))
 
 (defn ^:no-doc namespaces
   [& ns-names]
--- test/such/f_immigration.clj
+++ test/such/f_immigration.clj
@@ -1,3 +1,12 @@
 (ns such.f-immigration (:require [such.immigration :as immigrate])
   (:use midje.sweet))
 
+(immigrate/import-all-vars clojure.set)
+
+(fact
+  (subset? #{1} #{1 2}) => true)
+
+(immigrate/import-prefixed-vars clojure.string str-)
+
+(fact 
+  (str-trim "   f     ") => "f")
