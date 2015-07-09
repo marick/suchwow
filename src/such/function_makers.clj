107cbe849dc649bec781a225968861ddb3372166
Brian Marick <marick@exampler.com>
2015-07-09 12:52:35 -0500

    Add new function-makers

--- src/such/function_makers.clj
+++ src/such/function_makers.clj
@@ -34,3 +34,106 @@
         (cond (empty? preds)  false
               (candidate arg) true
               :else           (recur remainder))))))
+
+
+(defn wrap-pred-with-catcher
+  "Produces a new function. It returns whatever value `pred` does, except
+   that it traps exceptions and returns `false`.
+
+        ( (wrap-pred-with-catcher even?) 4) => true
+        
+        (even? :hi) => (throws)
+        ( (wrap-pred-with-catcher even?) :hi) => false
+"
+  [pred]
+  (fn [& xs]
+    (try (apply pred xs)
+    (catch Exception ex false))))
+
+(defn mkfn:lazyseq
+  "This is used to generate the `lazyseq:x->...` functions. See the source."
+  [prefixer]
+  (fn two-arg-form
+    ([transformer pred]
+       (fn lazily-handle [[x & xs :as lazyseq]]
+         (lazy-seq 
+          (cond (empty? lazyseq)
+                nil
+                          
+                (pred x)
+                ((prefixer x (transformer x)) (lazily-handle xs))
+                          
+                :else
+                (cons x (lazily-handle xs))))))
+    ([transformer]
+       (two-arg-form transformer (constantly true)))))
+
+(def ^{:arglists '([f] [f pred])}
+  lazyseq:x->abc
+  "Takes a transformer function and an optional predicate. 
+   The transformer function must produce a collection, call it `coll`. 
+   `pred` defaults to `(constantly true)`.
+   Produces a function that converts one lazy sequence into another.
+   For each element of the input sequence:
+   
+   * If `pred` is falsey, the unchanged element is in the output sequence.
+   
+   * If `pred` is truthy, the new `coll` is \"spliced\" into the output
+     sequence in place of the original element.
+    
+           (let [replace-with-N-copies (lazyseq:x->abc #(repeat % %))]
+             (replace-with-N-copies [0 1 2 3]) => [1 2 2 3 3 3])
+           
+           (let [replace-evens-with-N-copies (lazyseq:x->abc #(repeat % %) even?)]
+             (replace-evens-with-N-copies [0 1 2 3]) => [1 2 2 3])
+"
+  (mkfn:lazyseq (fn [x tx] #(concat tx %))))
+
+(def ^{:arglists '([f] [f pred])}
+  lazyseq:x->xabc
+  "The same behavior as [[lazyseq:x->abc]], except that the generated collection
+   is spliced in *after* the original element, rather than replacing it.
+    
+           (let [augment-with-N-negatives (lazyseq:x->xabc #(repeat % (- %)))]
+             (augment-with-N-negatives [0 1 2 3]) => [0 1 -1 2 -2 -2 3 -3 -3 -3])
+"           
+  (mkfn:lazyseq (fn [x tx] #(cons x (concat tx %)))))
+
+
+(def ^{:arglists '([f] [f pred])}
+  lazyseq:x->y
+  "Takes an arbitrary function and an optional predicate. 
+   `pred` defaults to `(constantly true)`.
+   Produces a function that converts one lazy sequence into another.
+   It differs from the input sequence in that elements for which `pred`
+   is truthy are replaced with `(f elt)`.
+    
+           (let [force-positive (lazyseq:x->y - neg?)]
+             (force-positive [0 1 -2 -3]) => [0 1 2 3])
+"
+  (mkfn:lazyseq (fn [x tx] #(cons tx %))))
+            
+
+
+            
+(defn lazyseq:criticize-deviationism
+  "Produces a function that inspects a given `coll` according to the `deviancy-detector`.
+   When a deviant element is found, the `reporter` is called for side-effect. It is given
+   the `coll` and the deviant element as arguments.
+   
+   All deviant elements are reported. The original collection is returned.
+    
+          (def bad-attitude? neg?)
+          (def flagged-negativity 
+               (lazyseq:criticize-deviationism (comp neg? second)
+                                               (fn [coll elt]
+                                                 (println \"Bad attitude from\" elt))))
+          
+          (def attitudes [ [:fred 32] [:joe 23] [:gary -10] [:brian -30] [:corey 10326] ])
+          (flagged-negativity attitudes) ;; :gary and :brian are flagged.
+"
+  [deviancy-detector reporter]
+  (fn [coll]
+    (doseq [x coll]
+      (when (deviancy-detector x) (reporter coll x)))
+    coll))
--- test/such/f_function_makers.clj
+++ test/such/f_function_makers.clj
@@ -14,3 +14,62 @@
   ;; Any empty list means that everything matches
   ((mkfn/any-pred) 3) => true)
 
+(fact wrap-pred-with-catcher
+  (let [wrapped (mkfn/wrap-pred-with-catcher even?)]
+    (wrapped 2) => true
+    (wrapped 3) => false
+    (even? nil) => (throws)
+    (wrapped nil) => false))
+  
+
+(fact "`lazyseq:x->abc` converts (possibly optionally) each element of a lazyseq and replaces it with N results"
+  (fact "one arg form processes each element"
+    ( (mkfn/lazyseq:x->abc #(repeat % %)) [1 2 3]) => [1 2 2 3 3 3])
+
+  (fact "two arg form processes only elements that match predicate"
+    ( (mkfn/lazyseq:x->abc #(repeat % %) even?) [1 2 3 4]) => [1 2 2 3 4 4 4 4])
+
+  (fact "empty sequences are handled"
+    ( (mkfn/lazyseq:x->abc #(repeat % %) even?) []) => empty?)
+
+  (fact "it is indeed lazy"
+    (let [made (mkfn/lazyseq:x->abc #(repeat % %) even?)]
+      (take 2 (made [0])) => empty?
+      (take 2 (made [0 1])) => [1]
+      (take 2 (made [0 1 2])) => [1 2]
+      (take 2 (made [0 1 2 3])) => [1 2]
+      (take 2 (made [0 1 2 3 4])) => [1 2]
+      (take 2 (made (range))) => [ 1 2 ]
+      (count (take 100000 (made (range)))) => 100000)))
+
+
+(fact "`lazyseq:x->xabc` converts (possibly optionally) each element of a lazyseq and replaces it
+       with N results. The first argument is preserved"
+  (fact "one arg form processes each element"
+    ( (mkfn/lazyseq:x->xabc #(repeat % (- %))) [1 2 3]) => [1 -1 2 -2 -2 3 -3 -3 -3])
+
+  (fact "two arg form processes only elements that match predicate"
+    ( (mkfn/lazyseq:x->xabc #(repeat % (- %)) even?) [1 2 3 4]) => [1 2 -2 -2 3 4 -4 -4 -4 -4]))
+
+
+(fact "`lazyseq:x->y` converts (possibly optionally) each element of a lazyseq and replaces it
+       with 1 result."
+  (fact "one arg form processes each element"
+    ( (mkfn/lazyseq:x->y -) [1 2 3]) => [-1 -2 -3 ])
+
+  (fact "two arg form processes only elements that match predicate"
+    ( (mkfn/lazyseq:x->y - even?) [1 2 3 4]) => [1 -2 3 -4]))
+
+
+(fact lazyseq:criticize-deviationism
+  (let [recorder (atom [])
+        messager #(format "%s - %s" %1 %2)
+        critiquer (mkfn/lazyseq:criticize-deviationism (comp neg? second)
+                                                       #(swap! recorder
+                                                               conj
+                                                               (messager %1 %2)))
+        data [[:ok 0] [:brian -1] [:corey 10326] [:gary -3]]]
+    (critiquer data) => data
+    @recorder => [(format "%s - %s" data [:brian -1])
+                  (format "%s - %s" data [:gary -3])]))
+                                                       
