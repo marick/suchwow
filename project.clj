70ec8adc01317f168a514fbd28b776a03b8f0d9f
Brian Marick <marick@exampler.com>
2015-07-11 15:31:51 -0500

    3.2.1 - adds more access to `readable` mechanism

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marick/suchwow "3.2.0"
+(defproject marick/suchwow "3.2.1"
   :description "Such functions! Such doc strings! Much utility!"
   :url "https://github.com/marick/suchwow"
   :pedantic? :warn
