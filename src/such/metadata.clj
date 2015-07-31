95a9e0b5344887449b45eafdd018d17760800126
Brian Marick <marick@exampler.com>
2015-07-31 17:17:05 -0500

    such.metadata

---
+++ src/such/metadata.clj
@@ -0,0 +1,29 @@
+(ns such.metadata
+  "Convenience functions for working with metadata. Intended to be used with
+  `(:require [such.metadata :as meta])`."
+  (:refer-clojure :exclude [merge assoc get]))
+
+(defn get
+  "Equivalent to `(get (meta o) k)` or `(get (meta o) k default)`."
+  ([o k default]
+     (clojure.core/get (meta o) k default))
+  ([o k]
+     (get o k nil)))
+
+(defn merge
+  "Merge the maps onto the metadata of `o`, creating a new object
+  equal to `o` but with the merged metadata.
+  
+      (meta/merge o {:meta-a 1})
+"
+  [o & maps]
+  (let [all (apply clojure.core/merge maps)]
+    (vary-meta o clojure.core/merge all)))
+
+(defn assoc
+  "`assoc` the key-value pairs onto the metadata of `o`, creating a
+  new object equal to `o` but with the new metadata."
+  [o & kvs]
+  (let [all (apply hash-map kvs)]
+    (merge o all)))
+
---
+++ test/such/f_metadata.clj
@@ -0,0 +1,45 @@
+(ns such.f-metadata
+  (:require [such.metadata :as meta])
+  (:use midje.sweet))
+
+
+(def o (with-meta [] {:meta :here}))
+
+(fact "get avoids needing to use `meta`"
+  (get (meta o) :meta) => :here
+  (meta/get o :meta) => :here
+
+  (get (meta o) :unknown) => nil
+  (meta/get o :unknown) => nil
+
+  (get (meta o) :unknown "default") => "default"
+  (meta/get o :unknown "default") => "default")
+
+(fact "merge avoids needing to use `meta`"
+  (let [oo (meta/merge o {:a 1})]
+    (meta oo) => {:meta :here, :a 1}
+    (= oo o) => true)
+
+  (fact "multiple arguments"
+    (let [oo (meta/merge o {:a 1} {:b 2})]
+      (meta oo) => {:meta :here, :a 1 :b 2}))
+
+  (fact "no arguments are required"
+    (let [oo (meta/merge o)]
+      (meta oo) => (meta o)
+      (identical? oo o) => false)))
+
+(fact "assoc avoids needing to use `meta`"
+  (let [oo (meta/assoc o :a 1)]
+    (meta oo) => {:meta :here, :a 1}
+    (= oo o) => true)
+
+  (fact "multiple arguments"
+    (let [oo (meta/assoc o :a 1 :b 2)]
+      (meta oo) => {:meta :here, :a 1 :b 2}))
+
+  (fact "no arguments are required"
+    (let [oo (meta/assoc o)]
+      (meta oo) => (meta o)
+      (identical? oo o) => false)))
+
