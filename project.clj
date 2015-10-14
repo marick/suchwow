3e563003a70440977a08a25c23f4e5c0cf75263b
Brian Marick <marick@exampler.com>
2015-10-14 17:00:01 -0500

    Test against clojure-1.8-beta

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
              :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-alpha5"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-beta1"]]}
              }
 
   :plugins [[lein-midje "3.2-RC4"]
