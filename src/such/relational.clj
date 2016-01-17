a398e5150632ae0c1bd43b0e170b9d4ea0c1c65e
Brian Marick <marick@exampler.com>
2016-01-17 17:35:48 -0600

    Finished rethink

--- src/such/relational.clj
+++ src/such/relational.clj
@@ -1,13 +1,22 @@
 (ns such.relational
-  "Given vectors of uniform maps, do some simple relational things to them.
+  "If you work with sequences of maps of the sort gotten by slurping
+   up relational database tables or CVS files, here are some functions
+   that allow you join-like behavior for one-to-one and one-to-many
+   relationships.
 
-   A work in progress. Note: will eventually contain the relational parts of
+   The core idea is that you create indexes for all the access patterns
+   you use often - as you would with relational database tables - and
+   use them to follow foreign keys and merge the results (approximating
+   a join).
+
+   A WORK IN PROGRESS. Note: will eventually contain the relational parts of
    clojure.set, with better documentation.
 
-   This is only available for Clojure 1.6 and later."
+   This is only available for Clojure 1.7 and later."
   (:require [clojure.set :as set]
             [such.maps :as map]
             [such.imperfection :refer :all]
+            [such.wrongness :refer [boom!]]
             [such.metadata :as meta]))
 
 (defn- force-sequential [v]
@@ -33,108 +42,148 @@
                                          (map prefixer (keys kvs))))]
       (set/rename-keys kvs translation))))
 
+(defn- option-controlled-merge [old new options]
+  (when (and (sequential? new)
+             (not (contains? options :into)))
+    (boom! "A merge using a one-to-many index must specify `:into`"))
+
+  (if-let [destination (:into options)]
+    (let [current (or (get old destination) [])
+          extended (into current new)]
+      (assoc old destination extended))
+    (merge old new)))
 
 ;;;; Public
 
+(defn one-to-one-index-on
+  "`table` should be a sequence of maps. `keyseq` is either a single value
+  (corresponding to a traditional `:id` or `:pk` entry) or a sequence of
+  values (corresponding to a compound key).
 
-(defn one-to-one-index-on [table keyseq]
+  The resulting index provides fast access to individual maps.
+
+      (def index:traditional (one-to-one-index-on table :id))
+      (select 5 :using index:traditional :only [:key-i-want])
+
+      (def index:compound (one-to-one-index-on table [\"intkey\" \"strkey\")))
+      (select [4 \"dawn\"] :using index:compound)
+
+  Note that keys need not be Clojure keywords.
+  "
+  [table keyseq]
   (if (sequential? keyseq)
     (-> table
         (set/index keyseq)
-        (meta/assoc ::type :compound-to-one
+        (meta/assoc ::type :one-to-one
                     ::value-handler first
+                    ::key-selector select-keys
+                    ::prefix-adder prefix-all-keys
                     ::key-maker (mkfn:key-for-index keyseq)))
     (one-to-one-index-on table [keyseq])))
 
-(defn select-map
+(defn one-to-many-index-on [table keyseq]
+  "`table` should be a sequence of maps. `keyseq` is either a single value
+  (corresponding to a traditional `:id` or `:pk` entry) or a sequence of
+  values (corresponding to a compound key).
+
+  The resulting index provides fast retrieval of vectors of matching maps.
+
+      (def index:traditional (one-to-many-index-on table :id))
+      (select 5 :using index:traditional :only [:key-i-want]) ; a vector of maps
+
+      (def index:compound (one-to-many-index-on table [\"intkey\" \"strkey\")))
+      (select [4 \"dawn\"] :using index:compound) ; a vector of maps
+
+  Keys may be either Clojure keywords or strings.
+  "
+  (if (sequential? keyseq)
+    (-> table
+        (set/index keyseq)
+        (meta/assoc ::type :one-to-many
+                    ::value-handler identity
+                    ::key-selector (fn [value keyseq]
+                                     (mapv #(select-keys % keyseq) value))
+                    ::prefix-adder (fn [value prefix]
+                                     (mapv #(prefix-all-keys % prefix) value))
+                    ::key-maker (mkfn:key-for-index keyseq)))
+    (one-to-many-index-on table [keyseq])))
+
+(defn select
+  "Produce a map by looking a key up in an index.
+
+  See <<someplace>> for examples.
+
+  `key` is a unique or compound key that's been indexed with [[one-to-one-index-on]]
+  or [[one-to-many-index-on]]. The `options` may be given as N keys and values
+  following `key` (Smalltalk style) or as a single map. They are:
+
+  :using <index>
+    (required) The index to use.
+  :only <[keys...]>
+    (optional) Keys you're interested in (default is all of them)
+  :prefix <prefix>
+    (optional) Prepend the given prefix to all the keys in the selected map.
+    The prefix may be either a string or keyword. The resulting key will be
+    of the same type (string or keyword) as the original.
+
+  The return value depends on the index. If it is `one-to-one`, a map is returned.
+  If it is `one-to-many`, a vector of maps is returned.
+  "
   ([key options]
      (assert (contains? options :using) "You must provide an index with `:using`.")
      (let [index (get options :using)
-           key-maker (meta/get index ::key-maker)
-           value-handler (meta/get index ::value-handler)
-           desired-keys (get options :only)
-           prefix (get options :prefix)]
+
+           [key-maker value-handler key-selector prefix-adder]
+           (mapv #(meta/get index %)
+                 [::key-maker ::value-handler ::key-selector ::prefix-adder])
+
+           [desired-keys prefix] (mapv #(get options %) [:only :prefix])]
+
        (-> index
            (get (key-maker (force-sequential key)))
            value-handler
-           (cond-> desired-keys (select-keys desired-keys))
-           (cond-> prefix (prefix-all-keys prefix)))))
+           (cond-> desired-keys (key-selector desired-keys))
+           (cond-> prefix (prefix-adder prefix)))))
   ([key k v & rest] ; k and v are to give this different arity than above
-     (select-map key (apply hash-map k v rest))))
+     (select key (apply hash-map k v rest))))
 
 
 (defn extend-map
+  "Add more key/value pairs to `kvs`. They are found by looking up values
+  in a [[one-to-one-index-on]] or [[one-to-many-index-on]] index.
+
+  See <<someplace>> for examples.
+
+  The `options` control what maps are returned and how they're merged into the
+  original `kvs`. They may be given as N keys and values
+  following `kvs` (Smalltalk style) or as a single map. They are:
+
+  :using <index>
+    (required) The index to use.
+  :via <key>
+    (required) A single foreign key or a sequence of them that is used to
+    look up a map in the <index>.
+  :into <key>
+    (optional, relevant only to a one-to-many map). Since a one-to-many map
+    can't be [[merge]]d into the `kvs`, it has to be added \"under\" (as the
+    value of) a particular `key`.
+  :only <[keys...]>
+    (optional) Keys you're interested in (default is all of them)
+  :prefix <prefix>
+    (optional) Prepend the given prefix to all the keys in the selected map.
+    The prefix may be either a string or keyword. The resulting key will be
+    of the same type (string or keyword) as the original.
+  "
+
   ([kvs options]
      (assert (contains? options :using) "You must provide an index with `:using`.")
      (assert (contains? options :via) "You must provide an index with `:using`.")
 
      (let [foreign-key-value (multi-get kvs (:via options))]
-       (merge kvs (select-map foreign-key-value options))))
+       (option-controlled-merge kvs
+                                (select foreign-key-value options)
+                                options)))
 
   ([kvs k v & rest] ; k and v are to give this different arity than above
      (extend-map kvs (apply hash-map k v rest))))
 
-
-
-
-;; (defn- one-to-N-index-on [one-key combining-fn maps prefix]
-;;   (let [result (reduce (fn [so-far kvs]
-;;                          (combining-fn so-far (get kvs one-key) kvs))
-;;                        {}
-;;                        maps)]
-;;         (cond-> result prefix (meta/assoc ::prefix (name prefix)))))
-
-;; (defn one-to-one-index-on
-;;   "Given a list of `maps` with a \"primary\" (unique) key,
-;;    produce a map from the different values of the primary key to
-;;    the original maps they identify.
-
-;;           (one-to-one-index-on :pk [{:pk 1, :rest 2} {:pk 2, :rest 3}])
-;;           => {1 {:pk 1, :rest 2}
-;;               2 {:pk 2, :rest 3}}
-;; "
-;;   ([one-key maps]
-;;      (one-to-one-index-on one-key maps nil))
-;;   ([one-key maps prefix]
-;;      (one-to-N-index-on one-key assoc maps prefix)))
-
-;; (defn one-to-many-index-on
-;;   ([one-key maps]
-;;      (one-to-many-index-on one-key maps nil))
-;;   ([one-key maps prefix]
-;;      (one-to-N-index-on one-key map/conj-into maps prefix)))
-
-;; (defn index-prefix [index]
-;;   (meta/get index ::prefix))
-
-;; (defn- rename-keys-according-to-index [index kvs]
-;;   (let [prefix (index-prefix index)
-;;         translation (apply hash-map (interleave (keys kvs)
-;;                                                 (map #(->> % name (str prefix) keyword)
-;;                                                      (keys kvs))))]
-;;     (set/rename-keys kvs translation)))
-
-
-;; (defn- merge-related-table*
-;;   [this-map this-foreign-key-name that-index-on-foreign-key key-selector-fn]
-;;   (->> (get this-map this-foreign-key-name)
-;;        (get that-index-on-foreign-key)
-;;        key-selector-fn
-;;        (rename-keys-according-to-index that-index-on-foreign-key)
-;;        (merge this-map)))
-
-
-
-;; (defn merge-related-table
-;;   ([this-map this-foreign-key-name that-index-on-foreign-key]
-;;      (merge-related-table* this-map this-foreign-key-name that-index-on-foreign-key identity))
-
-;;   ([this-map this-foreign-key-name that-index-on-foreign-key that-keys-wanted]
-;;      (merge-related-table* this-map this-foreign-key-name that-index-on-foreign-key
-;;                      #(select-keys % that-keys-wanted))))
-
-;; (defn flattening-lookup [starting-value starting-index first-key-selection & descents]
-;;   (reduce (fn [so-far [foreign-key relevant-index resulting-key-selection]]
-;;             (merge-related-table so-far foreign-key relevant-index resulting-key-selection))
-;;           (select-keys (get starting-index starting-value) first-key-selection)
-;;           descents))
--- test/such/f_relational.clj
+++ test/such/f_relational.clj
@@ -6,43 +6,63 @@
             [clojure.pprint :refer [pprint]]
             [midje.sweet :refer :all]))
 
-; (when>=1-6
+(when>=1-7
 
 
 (fact "indices"
   (fact "one-to-one indices"
     (let [data [{:id 1 :rest ..rest1..} {:id 2 :rest ..rest2..}]
           index (subject/one-to-one-index-on data :id)]
-      (subject/select-map 1 :using index) => {:id 1 :rest ..rest1..}))
+      (subject/select 1 :using index) => {:id 1 :rest ..rest1..}))
 
   (fact "one-to-one indices where the keys are compound"
     (let [data [{:id 1 :pk 1 :rest ..rest11..}
                 {:id 1 :pk 2 :rest ..rest12..}
                 {:id 2 :pk 2 :rest ..rest22..}]
           index (subject/one-to-one-index-on data [:id :pk])]
-      (subject/select-map [1 1] :using index) => (first data)
-      (subject/select-map [1 2] :using index) => (second data)))
+      (subject/select [1 1] :using index) => (first data)
+      (subject/select [1 2] :using index) => (second data)))
+
+  (fact "one-to-many indices"
+    (let [data [{:id 1 :rest ..rest11..}
+                {:id 1 :rest ..rest12..}
+                {:id 2 :rest ..rest22..}]
+          index (subject/one-to-many-index-on data :id)]
+      (subject/select 1 :using index) => (just (first data) (second data) :in-any-order)))
 
   (fact "options"
     (let [data [{:id 1 :rest ..rest1..} {:id 2 :rest ..rest2..}]
           index (subject/one-to-one-index-on data :id)]
       (fact "can limit the number of keys returned"
-        (subject/select-map 1 :using index :only [:rest]) => {:rest ..rest1..})
+        (subject/select 1 :using index :only [:rest]) => {:rest ..rest1..})
       (fact "can add a prefix to keys as keyword..."
-        (subject/select-map 1 :using index :prefix :pre-) => {:pre-id 1 :pre-rest ..rest1..})
+        (subject/select 1 :using index :prefix :pre-) => {:pre-id 1 :pre-rest ..rest1..})
       (fact "both"
-        (subject/select-map 1 :using index :only [:rest] :prefix :pre-) => {:pre-rest ..rest1..})
+        (subject/select 1 :using index :only [:rest] :prefix :pre-) => {:pre-rest ..rest1..})
 
       (fact "options can also be provided as maps"
-        (subject/select-map 1 {:using index :only [:rest]}) => {:rest ..rest1..})))
+        (subject/select 1 {:using index :only [:rest]}) => {:rest ..rest1..})))
+
+  (fact "options for one-to-many-maps"
+    (let [data [{:id 1 "rest" ..rest11..}
+                {:id 1 "rest" ..rest12..}
+                {:id 2 "rest" ..rest22..}]
+          index (subject/one-to-many-index-on data :id)]
+      (subject/select 1 :using index :only ["rest"]) => (just {"rest" ..rest11..}
+                                                                 {"rest" ..rest12..}
+                                                                 :in-any-order)
+      (subject/select 1 :using index :only ["rest"] :prefix "XX")
+      => (just {"XXrest" ..rest11..}
+               {"XXrest" ..rest12..}
+               :in-any-order)))
 
   (fact "works with string keys and prefixes"
     (let [data [{"id" 1 "rest" ..rest1..} {"id" 2 "rest" ..rest2..}]
           index (subject/one-to-one-index-on data "id")]
       (fact "both can be strings"
-        (subject/select-map 1 :using index :prefix "pre-") => {"pre-id" 1 "pre-rest" ..rest1..})
+        (subject/select 1 :using index :prefix "pre-") => {"pre-id" 1 "pre-rest" ..rest1..})
       (fact "note that it is the type of the original key that determines type of result key"
-        (subject/select-map 1 :using index :prefix :pre-) => {"pre-id" 1 "pre-rest" ..rest1..}))))
+        (subject/select 1 :using index :prefix :pre-) => {"pre-id" 1 "pre-rest" ..rest1..}))))
 
 
 (fact "you can extend maps via indexes"
@@ -60,162 +80,39 @@
           foreign-table [{:alpha "a" :id 1 :val "fa"} {:alpha "b" :id "2" :val "fb"}]
           foreign-index (subject/one-to-one-index-on foreign-table [:alpha :id])]
 
+      ;; TODO: This test fails under 1.6, but not 1.7 or 1.8.
       (subject/extend-map original-map :using foreign-index :via [:foreign_id_alpha :foreign_id_num]
                           :only [:val] :prefix "foreign-")
       => {:id 1 :foreign_id_alpha "a" :foreign_id_num 1 :rest ..rest1..
           :foreign-val "fa"}))
 
 
+  (fact "one-to-many tables merge under a given key"
+    (let [foreign-table [{:id "a" :val "fa"} {:id "a" :val "fb"}]
+          foreign-index (subject/one-to-many-index-on foreign-table :id)]
+
+      (fact "you can add the key"
+        (let [original-map {:id 1 :foreign_id "a" :rest ..rest1..}
+              result (subject/extend-map original-map :using foreign-index :via :foreign_id
+                                         :into :foreign-data
+                                         :only [:val] :prefix "f-")]
+
+          result => (contains original-map)
+          (:foreign-data result) => (just [{:f-val "fa"} {:f-val "fb"}]
+                                          :in-any-order)))
+
+      (fact "you can append to existing values"
+        (let [original-map {:id 1 :foreign_id "a" :rest ..rest1..
+                            :foreign-data ["already here"]}
+              result (subject/extend-map original-map :using foreign-index :via :foreign_id
+                                         :into :foreign-data
+                                         :only [:val] :prefix "f-")]
+          (:foreign-data result) => (just ["already here" {:f-val "fa"} {:f-val "fb"}]
+                                          :in-any-order)))))
+
   )
 
 
+(future-fact "error handling")
 
-;; (facts "about extracting a simple one-to-one index from maps"
-;;   (subject/one-to-one-index-on :pk [{:pk 1, :rest 2} {:pk 2, :rest 3}])
-;;   => {1 {:pk 1, :rest 2}
-;;       2 {:pk 2, :rest 3}}
-
-;;   (fact "works with non-keyword keys"
-;;     (subject/one-to-one-index-on "i" [{"i" 1, :rest 2} {"i" 2, :rest 3}])
-;;     => {1 {"i" 1, :rest 2}
-;;         2 {"i" 2, :rest 3}})
-
-;;   (fact "you can also stash a prefix string for future use"
-;;     (let [result (subject/one-to-one-index-on :pk [{:pk 1, :rest 2} {:pk 2, :rest 3}] :pre-)]
-;;       (subject/index-prefix result) => "pre-")) ; note stringification
-
-;;   (fact "a prefix can also be a string"
-;;     (let [result (subject/one-to-one-index-on :pk [{:pk 1, :rest 2} {:pk 2, :rest 3}] "pre-")]
-;;       (subject/index-prefix result) => "pre-")))
-
-;; (facts "about extracting a one-to-many index from maps"
-;;   ;; Simple test, since it shares code with above.
-;;   (let [r (subject/one-to-many-index-on :foreign_id
-;;                                         [{:pk 1, :foreign_id 2}
-;;                                          {:pk 2, :foreign_id 2}
-;;                                          {:pk 3, :foreign_id 8}])]
-;;     (get r 2) => (just [{:pk 1, :foreign_id 2} {:pk 2, :foreign_id 2}] :in-any-order)
-;;     (get r 8) => (just [{:pk 3, :foreign_id 8}] :in-any-order)))
-
-
-;; (facts "about merging via a foreign key"
-;;   (let [subtable [{:subpk ..sub1.. :other ..other1..} {:subpk ..sub2.. :other ..other2..}]]
-;;     (fact "a starting - not so useful - version"
-;;       (let [sub-index (subject/one-to-one-index-on :subpk subtable)]
-;;         (subject/merge-related-table {:pk ..pk.. :foreign ..sub1..} :foreign sub-index)
-;;         => {:pk ..pk.. :foreign ..sub1.. :subpk ..sub1.. :other ..other1..}))
-
-;;     (fact "it is better if the map contains a prefix"
-;;       (let [sub-index (subject/one-to-one-index-on :subpk subtable :sub_)]
-;;         (subject/merge-related-table {:pk ..pk.. :foreign ..sub1..} :foreign sub-index)
-;;         => {:pk ..pk.. :foreign ..sub1.. :sub_subpk ..sub1.. :sub_other ..other1..}))
-
-;;     (fact "you can also select a subset of the foreign map's keys"
-;;       (let [sub-index (subject/one-to-one-index-on :subpk subtable)]
-;;         (subject/merge-related-table {:pk ..pk.. :foreign ..sub1..} :foreign sub-index [:other])
-;;         => {:pk ..pk.. :foreign ..sub1.. :other ..other1..}))))
-
-
-;; (facts "about flattening lookup"
-;;   (let [problems [{:id 1, :name "problem1"}
-;;                   {:id 2, :name "problem2"}]
-;;         index:problem-by-id (subject/one-to-one-index-on :id problems "problem_")
-;;         icd10-codes [{:code "one", :description "code1"}
-;;                      {:code "two", :description "code2"}
-;;                      {:code "three", :description "code3"}]
-;;         index:icd10-by-code (subject/one-to-one-index-on :code icd10-codes "icd10_")
-;;         icd10-code-problem-assignments [{:id 1, :problem_id 1, :code "one"}
-;;                                         {:id 2, :problem_id 2, :code "two"}
-;;                                         {:id 3, :problem_id 2, :code "three"}]
-;;         index:icd10-code->problem-id (subject/one-to-one-index-on :code icd10-code-problem-assignments)]
-
-;;     (subject/flattening-lookup 1 index:problem-by-id [:name]) => {:name "problem1"}
-
-;;     (subject/flattening-lookup "one" index:icd10-code->problem-id [:problem_id :code]
-;;                                [:problem_id index:problem-by-id [:name]])
-;;     => {:problem_id 1, :code "one", :problem_name "problem1"}
-
-;;     (subject/flattening-lookup "one" index:icd10-code->problem-id [:problem_id :code]
-;;                                [:problem_id index:problem-by-id [:name]]
-;;                                [:code index:icd10-by-code [:description]])
-;;     => {:problem_id 1, :code "one", :problem_name "problem1", :icd10_description "code1"}))
-
-;) ; when>=1-6
-
-;(when>=1-7 ; this uses structural-typing, which requires 1-7
-
-;; (require '[structural-typing.type :as type])
-
-
-;; ;;; Here is the construction of a (medical) Problem from an ICD-10 code.
-;; ;;; https://en.wikipedia.org/wiki/ICD-10
-
-;; ;;; Imagine this is found in a namespace that has authority over the construction of
-;; ;;; Problem maps. Its various `from-` functions may traverse foreign keys into different
-;; ;;; tables to assemble the maps. The end result must have these keys:
-
-;; (def problem-keys [:problem_id :problem_name :icd10_code :icd10_description])
-
-;; ;;; ... which is checked like this:
-
-;; (def type-repo (-> type/empty-type-repo
-;;                    (type/replace-error-handler type/throwing-error-handler)
-;;                    (type/named :Problem (apply type/requires problem-keys))))
-;; (def this-is-a-problem #(type/built-like type-repo :Problem %))
-
-
-;; ;;; We work from relational tables (vectors of maps) that have been
-;; ;;; fetched from a relational database or constructed by hand.
-
-;; (def problems [{:id 1, :name "problem1"}
-;;                {:id 2, :name "problem2"}])
-
-;; (def icd10-codes [{:code "one", :description "code1"}
-;;                   {:code "two", :description "code2"}
-;;                   {:code "three", :description "code3"}])
-
-;; (def icd10-code-problem-assignments [{:id 1, :code "one",  :problem_id 1}
-;;                                      {:id 2, :code "two",  :problem_id 2}
-;;                                      ;; Note next one is broken
-;;                                      {:id 3, :code "three" :problem_id nil}])
-
-
-;; ;;; For each table, we can construct "indexes" that provide fast access to
-;; ;;; individual maps within the "tables". When elements are fetched via the
-;; ;;; index, they can have a prefix prepended to them. (Think of `SELECT table.column AS col`,
-;; ;;; except enforced for all queries.)
-
-;; (def index:problem-by-id (subject/one-to-one-index-on :id problems "problem_"))
-;; (def index:icd10-by-code (subject/one-to-one-index-on :code icd10-codes "icd10_"))
-;; (def index:icd10-code->problem-id  ; this contributes nothing to the final problem, so no prefix.
-;;   (subject/one-to-one-index-on :code icd10-code-problem-assignments))
-
-;; ;;; `from` functions build up a result in the following stages:
-;; ;;;   1. Select the initial map from the value of a key in an index.
-;; ;;;   2.   Repeat: follow a foreign key and merge selected key/values.
-;; ;;;   3. Rename any keys that are not to taste.
-;; ;;;   4. Restrict the resulting map to the keys allowed in a Problem.
-;; ;;;   5. Blow up unless the Problem is well-formed.
-
-;; ;;; Note: in each step of the process, there's no equivalent to `SELECT *`. My experience
-;; ;;; has been that it's a lot easier to understand what's going on if you can see where
-;; ;;; each key is coming from, especially if the original column names are imperfect.
-
-;; (defn from-icd10
-;;   [icd10-code]
-;;   (-> icd10-code
-;;       (subject/flattening-lookup
-;;          index:icd10-code->problem-id [:problem_id :code]
-;;          [:problem_id index:problem-by-id [:name]]
-;;          [:code index:icd10-by-code [:description]])
-;;       (set/rename-keys {:code :icd10_code})
-;;       (select-keys problem-keys)
-;;       this-is-a-problem))
-
-;; (fact
-;;   (from-icd10 "one") => {:problem_id 1 :problem_name "problem1",
-;;                          :icd10_code "one", :icd10_description "code1"}
-;;   (from-icd10 "three") => (throws #":problem_id must exist and be non-nil"))
-
-
-;) ; >= 1.7
+) ; when>=1-7
